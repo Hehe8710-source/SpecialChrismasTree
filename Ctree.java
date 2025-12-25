dfc08791f0a711ffeb23db560ce57059b8ced8f2
Hehe8710-source <Hehe8710@outlook.com>
2025-12-25 11:23:26 +0800

    Add files via upload

---
+++ Ctree.java
@@ -0,0 +1,19 @@
+public class Ctree {
+    public static void main(String[] args) {
+        String[] lines = {
+                "             ★",
+                "            /  \\",
+                "           / \\/ \\",
+                "          /_\\/\\/_\\",
+                "         /_ ^^ _/_\\",
+                "        /_\\/_\\/_\\/_\\",
+                "       /\\/_\\/_\\/_\\/_\\",
+                "          |||||||",
+                "          |||||||",
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
